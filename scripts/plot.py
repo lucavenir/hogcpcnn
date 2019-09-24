7f475b7a354dd778a00f1fcd9004789178ca7c2d
lucavenir <luca.venir@studenti.unipd.it>
2019-09-24 10:59:57 +0200

    More deltas, final plotting

--- scripts/plot.py
+++ scripts/plot.py
@@ -7,6 +7,11 @@
 #   - 0.0055 (approx. 10 times more)
 #   - 0.067 (approx. 10 times more)
 #   - 0.019196354 (geometric mean between the last two)
+# EXPERIMENTING:
+#   - 0.1005,
+#   - 0.134,
+#   - 0.15075,
+#   - 0.22612
 #   - TBD: delta value to be computed for each motif like they did on the paper
 
 import matplotlib.pyplot as plt
@@ -33,7 +38,17 @@ colours = [
     'y',
     'k',
 ]
-delta = 0.000496
+deltas = [
+    0.000496,  # (paper value)
+    0.0055,  # (approx. 10 times more)
+    0.067,  # (approx. 10 times more)
+    0.019196354,  # (geometric mean between the last two)
+    # PROVA
+    0.1005,
+    0.134,
+    0.15075,
+    0.22612
+]
 
 # The OUTPUT files of this script should be the following:
 output_folder = '../out/plots/'
@@ -43,53 +58,57 @@ def plotting():
     global input_folder
     global database
     global motifs
-    global delta
+    global deltas
     global output_folder
     global colours
 
-    # File locations
-    input_folder += database+'/delta='+delta+'/'
+    for delta in deltas:
+        # File locations
+        in_f = input_folder+database+'/delta='+str(delta)+'/'
 
-    try:
-        os.mkdir(output_folder)
-    except FileExistsError:
-        pass
+        try:
+            os.mkdir(output_folder)
+        except FileExistsError:
+            pass
 
-    tpr_values = dict()
-    fpr_values = dict()
-    tprfpr_ks = dict()
-    for motif in motifs:
-        tprfpr_file = 'tprfpr_'+motif+'_delta='+delta+'.txt'
-        tprfpr_ks[motif] = list()
-        tpr_values[motif] = list()
-        fpr_values[motif] = list()
+        tpr_values = dict()
+        fpr_values = dict()
+        tprfpr_ks = dict()
+        for motif in motifs:
+            tprfpr_file = 'tprfpr_'+motif+'_delta='+str(delta)+'.txt'
+            tprfpr_ks[motif] = list()
+            tpr_values[motif] = list()
+            fpr_values[motif] = list()
 
-        with open(input_folder+tprfpr_file, 'r') as infp:
-            lines = infp.readlines()
-            for l in lines:
-                s = l.split(' ')
-                k = int(s[0].split('=')[1].split(':')[0])
-                s1 = s.split(',')
-                tpr = float(s1[0])
-                fpr = float(s1[1])
+            with open(in_f+tprfpr_file, 'r') as infp:
+                lines = infp.readlines()
+                for l in lines:
+                    s = l.split(' ')
+                    k = int(s[0].split('=')[1].split(':')[0])
+                    s1 = s[1].split(',')
+                    tpr = float(s1[0])
+                    fpr = float(s1[1])
 
-                tprfpr_ks[motif].append(k)
-                tpr_values[motif].append(tpr)
-                fpr_values[motif].append(fpr)
+                    tprfpr_ks[motif].append(k)
+                    tpr_values[motif].append(tpr)
+                    fpr_values[motif].append(fpr)
 
-    plt.title('Results with delta='+delta)
-    plt.ylabel('TPR')
-    plt.xlabel('FPR')
-    plt.axis([0,1,0,1])
+        plt.title('Results with delta='+str(delta))
+        plt.ylabel('TPR')
+        plt.xlabel('FPR')
+        # plt.axis([0,0.08,0,1])
 
-    for i, motif in enumerate(motifs):
-        tpr = tpr_values[motif]
-        fpr = fpr_values[motif]
-        k = tprfpr_ks[motif]
-        plt.plot(fpr, tpr, fmt='-'+colours[i], label=motif)
+        for i, motif in enumerate(motifs):
+            # Extracting series, removing "0"
+            tpr = tpr_values[motif][:-1]
+            fpr = fpr_values[motif][:-1]
+            k = tprfpr_ks[motif]
+            plt.plot(fpr, tpr, '^-'+colours[i], label=motif)
+            for i in range(len(tpr)):
+                plt.annotate(k[i], (fpr[i], tpr[i]))
+            plt.legend()
 
-    plt.legend()
-    plt.show()
+        plt.show()
 
 if __name__ == "__main__":
     plotting()
